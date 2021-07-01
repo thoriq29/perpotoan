613c299a4adccaa935474bc2749f5a50a3dca35d
Thoriqul Azis <39757739+thoriq29@users.noreply.github.com>
2021-07-01 10:38:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.get(routePath.store, (req, res) => {
     res.sendFile(__dirname+viewPaths.store)
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
